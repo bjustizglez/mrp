6c4da52578efc6fff166104384bb7f30959a3473
Bernardo Justiz González <67840046+bjustizglez@users.noreply.github.com>
2020-11-30 19:48:19 -0500

    actualizando manifest

--- price_mrp/__manifest__.py
+++ price_mrp/__manifest__.py
@@ -4,10 +4,14 @@
 {
     'name': 'Sale Prices - Manufacturing',
     'category': 'Manufacturing/Manufacturing',
-    'summary': 'Sale Prices from Manufacturing',
+    'summary': 'Sale Prices from Manufacturing bill of material',
+    'author': 'Xookweb',
     'version': '1.0',
     'website': 'https://github.com/bjustizglez/mrp',
     'description': "Sale Prices from Manufacturing",
+    'license': 'OPL-1',
+    'currency': 'EUR',
+    'price': 15.0,
     'depends': ['mrp'],
     'data': [
         'views/product_views.xml',
